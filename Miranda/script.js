cd96568c6c11b18bbc8f022e4cea8a35cad5a895
JATIN <PATELJATINPC@gmail.com>
2023-06-28 02:33:38 +0530

    project completed adding some new container in page 2 also some bug fix.

--- Miranda/script.js
+++ Miranda/script.js
@@ -1,2 +1,18 @@
-console.log("Hello WOrld")
+var t1 = gsap.timeline()
 
+t1.to("#page1",{
+    y:"100vh",
+    scale:0.5,
+    duration:0.2,
+})
+t1.to("#page1",{
+    y:"-100vh",
+    duration:1.5,
+}) 
+t1.to("#page1",{
+    rotate:-360,
+    duration:1,
+    delay:0.5,
+    y:"0vh",
+    scale:1,
+})
