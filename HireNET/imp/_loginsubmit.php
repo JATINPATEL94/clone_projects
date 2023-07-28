67ba5e95cb12f49b3e956b8ceb3ed2597e1c7923
JATINPATEL94 <pateljatinpc@gmail.com>
2023-07-29 00:15:42 +0530

    connect to database and fixing some errors

--- HireNET/imp/_loginsubmit.php
+++ HireNET/imp/_loginsubmit.php
@@ -1,38 +1,33 @@
-<?php 
-echo ' 
-<head>
-   <meta http-equiv="refresh" content="0,https://hirenet.orgfree.com/login.html">
-</head>
-';
-
+<?php
 include '_dbconnect.php';
 $login = false;
 
-
-$username = $_POST['name'];
+$username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$hash = password_hash($password, PASSWORD_DEFAULT);
-$sql = 'SELECT * FROM `users` WHERE password="$hash"';
+$sql = "SELECT * FROM `users` WHERE email='$email'";
 $result = mysqli_query($conn, $sql);
-echo $result;
-if ($result){
-    $login = true;
-    session_start();
-    $_SESSION['loggedin'] = true;
-    $_SESSION['username'] = $username;
-    header("location: https://hirenet.orgfree.com");
 
-} 
-
-else{
-     echo "<script>
-    alert('Invalid Credentials. We are redirecting you back to login page.');
-    </script>"; 
+if ($result && mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $hash = $row['password'];
+    if (password_verify($password, $hash)) {
+        $login = true;
+        session_start();
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $username;
+        header("location: https://hirenet.orgfree.com");
+    } else {
+        echo "<script>
+            alert('Invalid Credentials. We are redirecting you back to the login page.');
+            window.location.href = '_login.html';
+        </script>";
+    }
+} else {
+    echo "<script>
+        alert('Invalid Credentials. We are redirecting you back to the login page.');
+        window.location.href = '_login.html';
+    </script>";
 }
-
-
-
-
-?>	
+?>
