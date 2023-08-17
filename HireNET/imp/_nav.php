4c11fc59f71996bf25b7b7deb9860d94455cdf6b
JATINPATEL94 <pateljatinpc@gmail.com>
2023-08-17 23:10:19 +0530

    make it responsive website

--- HireNET/imp/_nav.php
+++ HireNET/imp/_nav.php
@@ -1,18 +1,25 @@
 <?php
 session_start();
-echo '<img src="images/logo.webp" alt="logo">
+?>
+<img src="images/logo.webp" alt="logo">
 <nav class="navbar">
     <ul class="navbar-list">
         <li><a href="/index.php" class="navbar-link Home">Home</a></li>
-        <li><a href="/jobs.php" class="navbar-link ">Jobs</a></li>
-        <li><a href="/index.php#Services" class="navbar-link ">About</a></li>
-        <li><a href="/index.php#Services" class="navbar-link ">Services</a></li>
-        <li><a href="/index.php#Contact" class="navbar-link ">Contact</a></li>';
-        if ($_SESSION['loggedin']) {
-            echo '<li><a href="/logout.php" class="navbar-link ">Logout</a></li>';
+        <li><a href="/jobs.php" class="navbar-link">Jobs</a></li>
+        <li><a href="/index.php#Services" class="navbar-link">About</a></li>
+        <li><a href="/index.php#Services" class="navbar-link">Services</a></li>
+        <li><a href="/index.php#Contact" class="navbar-link">Contact</a></li>
+        <?php
+        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
+            echo '<li><a href="/logout.php" class="navbar-link">Logout</a></li>';
+        } else {
+            echo '<li><a href="/login.php" class="navbar-link">Login</a></li>';
         }
-        else {
-            echo '<li><a href="/login.php" class="navbar-link ">Login</a></li>';
-        }
-        echo '</ul></nav>';
-?>
+        ?>
+    </ul>
+</nav>
+
+<div class="mobile-navbar-btn">
+    <i class="ri-menu-3-line mobile-navbar-icon"></i>
+    <i class="ri-menu-unfold-line mobile-navbar-icon"></i>
+</div>
